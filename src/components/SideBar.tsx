b3587b96aeb3b1ac3fe73a4023c0c98aab133801
guivictorr <55333929+guivictorr@users.noreply.github.com>
2022-05-09 14:23:21 -0300

    challenge resolution

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { GenreResponseProps } from "../App";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+type SideBarProps = {
+  selectedGenreId: number;
+  onChangeGenre: (id: number) => void;
+}
+
+export function SideBar({ onChangeGenre, selectedGenreId }: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onChangeGenre(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
